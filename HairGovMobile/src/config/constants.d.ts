28000c7bfa069e3e8a9a02515aa277afe9bcc4fa
Kassi_0958 <you@example.com>
2025-11-03 19:54:26 +0100

    Réorganisation de la structure : déplacement du contenu de backend à la racine

--- HairGovMobile/src/config/constants.d.ts
+++
@@ -1,9 +0,0 @@
-declare const API_URL: string;
-
-declare const STORAGE_KEYS: {
-  readonly USER_TOKEN: 'userToken';
-  readonly USER_DATA: 'userData';
-  readonly GUEST_SESSION: 'guestSession';
-};
-
-export { API_URL, STORAGE_KEYS };
